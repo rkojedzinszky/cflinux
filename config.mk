e0ff7fd1cde73e86b707dd4a56b7cbfaa0d70a24
krichy <krichy>
2004-02-18 18:32:27 +0000

    Upgraded to the Linux kernel 2.4.25.

--- config.mk
+++ config.mk
@@ -36,7 +36,7 @@ export UC_ENV
 export UC_PATH
 
 # The version of the used kernel
-KERNEL_VERSION := 2.4.24
+KERNEL_VERSION := 2.4.25
 
 # the downloads dir, where the sources will reside
 SOURCES_DIR = $(TOP_DIR)/sources
