bfd661a53e5154965eff9f317f1955a061ff4360
krichy <krichy>
2004-10-01 15:33:26 +0000

    Updated to support 'KICK' to kick a logged in user.

--- fs_config/ppp/upd_qos.sh
+++ fs_config/ppp/upd_qos.sh
@@ -4,4 +4,11 @@ if=`grep -H "^User-Name $1$" /var/run/radattr.* 2>/dev/null | \
 	sed "s/^.*radattr\.//;s/:User-Name $1$//"`
 [ -z "$if" ] && exit 0
 
-/etc/ppp/set_qos.sh "$if" "$2" "$2"
+case "$4" in
+	KICK)
+		exec kill $(cat /var/run/$if.pid)
+	;;
+	*)
+		exec /etc/ppp/set_qos.sh "$if" "$2" "$2"
+	;;
+esac
