26a64cb5a3bd4e51cae7c2ac23cfecab347947b6
krichy <krichy>
2005-04-29 14:21:09 +0000

    Upgraded the linux kernel and grsec together.

--- config.mk
+++ config.mk
@@ -36,7 +36,7 @@ export UC_ENV
 export UC_PATH
 
 # The version of the used kernel
-KERNEL_VERSION := 2.4.29
+KERNEL_VERSION := 2.4.30
 
 # the downloads dir, where the sources will reside
 SOURCES_DIR = $(TOP_DIR)/sources
--- mk/grsec.mk
+++ mk/grsec.mk
@@ -18,8 +18,8 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 PKG := grsec
-SRC_FILENAME = grsecurity-2.1.1-2.4.29.tar.bz2
-EXTRACTED_DIR = grsecurity-2.1.1-2.4.29
+SRC_FILENAME = grsecurity-2.1.5-2.4.30.tar.bz2
+EXTRACTED_DIR = grsecurity-2.1.5-2.4.30
 DOWNLOAD_SITES = $(CFLINUX_PACKAGES)
 
 # include the common package targets 
