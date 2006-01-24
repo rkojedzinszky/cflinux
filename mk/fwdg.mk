e9a6bb17fd138718fd70955b838f8d15f8c7933b
krichy <krichy>
2006-01-24 08:07:12 +0000

    Upgraded fwdg again to the latest.

--- mk/fwdg.mk
+++ mk/fwdg.mk
@@ -18,8 +18,8 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 PKG := fwdg
-SRC_FILENAME = fwdg-r18.tar.bz2
-EXTRACTED_DIR = fwdg-r18
+SRC_FILENAME = fwdg-r19.tar.bz2
+EXTRACTED_DIR = fwdg-r19
 DOWNLOAD_SITES = $(CFLINUX_PACKAGES)
 
 # include the common package targets 
