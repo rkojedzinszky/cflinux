f7c5fbc62c877e3c566bc2529388a3b6f1f215fb
krichy <krichy>
2006-04-27 14:14:54 +0000

    Removed halt and poweroff commands from busybox, as cflinux does not need them.

--- mk/busybox.mk
+++ mk/busybox.mk
@@ -29,7 +29,8 @@ PATCHES = busybox.init.patch \
 	busybox.pwd2spwd.c.patch \
 	busybox.ifenslave.patch \
 	busybox.httpd_c.patch \
-	busybox.wget_c.patch
+	busybox.wget_c.patch \
+	busybox.no_halt_poweroff.patch
 
 # include the common package targets 
 include $(TOP_DIR)/packages.mk 
