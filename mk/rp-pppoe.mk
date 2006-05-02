8b959113ba40ce90b087a2ebd4eb8dd32b8b92ed
krichy <krichy>
2006-05-02 16:48:35 +0000

    Increased the MAX_INTERFACES in rp-pppoe's relay.h.

--- mk/rp-pppoe.mk
+++ mk/rp-pppoe.mk
@@ -29,7 +29,8 @@ PATCHES = rp-pppoe.patch \
 	rp-pppoe.wild_ifname.patch \
 	rp-pppoe.relay.patch \
 	rp-pppoe.dont_close_fds.patch \
-	rp-pppoe.relay.c_padi_no_warn.patch
+	rp-pppoe.relay.c_padi_no_warn.patch \
+	rp-pppoe.relay_h_maxinterfaces.patch
 
 # include the common package targets 
 include $(TOP_DIR)/packages.mk 
