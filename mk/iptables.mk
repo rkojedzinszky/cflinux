1f6186a89ba8c2650a93878925a7234ff85adcf3
krichy <krichy>
2006-10-13 10:10:10 +0000

    Upgraded iptables to 1.3.6.

--- mk/iptables.mk
+++ mk/iptables.mk
@@ -18,8 +18,8 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 PKG := iptables
-SRC_FILENAME = iptables-1.3.5.tar.bz2
-EXTRACTED_DIR = iptables-1.3.5
+SRC_FILENAME = iptables-1.3.6.tar.bz2
+EXTRACTED_DIR = iptables-1.3.6
 DOWNLOAD_SITES = http://www.netfilter.org/projects/iptables/files \
 		$(CFLINUX_PACKAGES)
 
