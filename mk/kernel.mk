bcdd2fe600323fdc25d180827dc9f940dab78669
krichy <krichy>
2006-10-28 23:06:33 +0000

    A radix fib implementation is ready to be tested. Probably, bugs will appear around locking, in insert, delete or flush, or somewhere else. Needs much much testing.

--- mk/kernel.mk
+++ mk/kernel.mk
@@ -36,7 +36,8 @@ PATCHES = kernel.vlan_mtu.patch \
 	kernel.igmp.c.max_membership.patch \
 	kernel.ipv4_no_rt_cache.patch \
 	kernel.ip_multitable.patch \
-	kernel.dst_input_no_loop.patch
+	kernel.dst_input_no_loop.patch \
+	kernel.fib_radix.patch
 
 # include the common package targets 
 include $(TOP_DIR)/packages.mk 
