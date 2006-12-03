14b39a151cc9500822379dc668171dada3eb84a5
krichy <krichy>
2006-12-03 17:16:49 +0000

    Made kfree_skb inline.

--- mk/kernel.mk
+++ mk/kernel.mk
@@ -37,7 +37,8 @@ PATCHES = kernel.vlan_mtu.patch \
 	kernel.lef.patch \
 	kernel.ip_multitable.patch \
 	kernel.dst_input_no_loop.patch \
-	kernel.fib_radix.patch
+	kernel.fib_radix.patch \
+	kernel.inline_kfree_skb.patch
 
 # include the common package targets 
 include $(TOP_DIR)/packages.mk 
