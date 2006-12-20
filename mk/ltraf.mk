5018661e8c4e392e2f997d18d1288a59251fb89e
krichy <krichy>
2006-12-20 15:00:59 +0000

    Upgraded ltraf to svn rev 163.

--- mk/ltraf.mk
+++ mk/ltraf.mk
@@ -18,8 +18,8 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 PKG := ltraf
-SRC_FILENAME = ltraf-r137.tar.bz2
-EXTRACTED_DIR = ltraf-r137
+SRC_FILENAME = ltraf-r163.tar.bz2
+EXTRACTED_DIR = ltraf-r163
 DOWNLOAD_SITES = $(CFLINUX_PACKAGES)
 
 # include the common package targets 
