71907e9e3cd8c6c6bdd8144d91f9452dc2afcaa1
krichy <krichy>
2007-05-21 11:43:12 +0000

    Upgraded quagga to 0.99.7.

--- mk/quagga.mk
+++ mk/quagga.mk
@@ -18,8 +18,8 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 PKG := quagga
-SRC_FILENAME = quagga-0.99.6.tar.gz
-EXTRACTED_DIR = quagga-0.99.6
+SRC_FILENAME = quagga-0.99.7.tar.gz
+EXTRACTED_DIR = quagga-0.99.7
 DOWNLOAD_SITES = http://www.quagga.net/download/ \
 		$(CFLINUX_PACKAGES)
 PATCHES = quagga.patch
