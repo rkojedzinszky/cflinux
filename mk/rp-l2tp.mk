4bbe261cc6ba38f9d92c6231949507ec326cb1e2
Richard Kojedzinszky <krichy@cflinux.hu>
2007-09-01 14:25:01 +0200

    Made rp-l2tp cross-compile.

--- mk/rp-l2tp.mk
+++ mk/rp-l2tp.mk
@@ -33,7 +33,7 @@ include $(TOP_DIR)/packages.mk
 configure: patch $(CONFIGURED_STAMP)
 
 $(CONFIGURED_STAMP):
-	(cd $(PKG_ROOT) && $(UC_PATH) ./configure \
+	(cd $(PKG_ROOT) && $(UC_PATH) ./configure --host=$(TARGET_HOST) \
 	 --prefix=/usr)
 	touch $(CONFIGURED_STAMP)
 
