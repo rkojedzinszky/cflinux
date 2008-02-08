1dce2f4ca57e4369737251bb683f0affea340e41
Richard Kojedzinszky <krichy@tvnetwork.hu>
2008-02-08 19:01:06 +0100

    Tuned rp-pppoe for the new kernel.

--- mk/rp-pppoe.mk
+++ mk/rp-pppoe.mk
@@ -30,7 +30,8 @@ PATCHES = rp-pppoe.patch \
 	rp-pppoe.dont_close_fds.patch \
 	rp-pppoe.relay.c_padi_no_warn.patch \
 	rp-pppoe.relay_h_maxinterfaces.patch \
-	rp-pppoe.relay_stat.patch
+	rp-pppoe.relay_stat.patch \
+	rp-pppoe.plugin_type.patch
 
 # include the common package targets 
 include $(TOP_DIR)/packages.mk 
