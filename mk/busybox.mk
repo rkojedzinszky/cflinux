a81e860a9948518ec99421db4f0e5f3f1aa61e6a
Richard Kojedzinszky <krichy@cflinux.hu>
2010-09-12 23:27:13 +0200

    Busybox upgraded to 1.17.2

--- mk/busybox.mk
+++ mk/busybox.mk
@@ -18,8 +18,8 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 PKG := busybox
-SRC_FILENAME = busybox-1.15.2.tar.bz2
-EXTRACTED_DIR = busybox-1.15.2
+SRC_FILENAME = busybox-1.17.2.tar.bz2
+EXTRACTED_DIR = busybox-1.17.2
 DOWNLOAD_SITES = http://busybox.net/downloads/ \
 		$(CFLINUX_PACKAGES)
 
@@ -27,9 +27,7 @@ PATCHES = busybox.init.patch \
 	busybox.no_static_libgcc.patch \
 	busybox.rdate.patch \
 	busybox.wget_c.patch \
-	busybox.no_halt_poweroff.patch \
 	busybox.syslogd_c_use__path_log.patch \
-	busybox.ps_opt_hack.patch
 
 # include the common package targets 
 include $(TOP_DIR)/packages.mk 
