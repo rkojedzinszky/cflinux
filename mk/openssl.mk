c8f7ccdf99a751a7e758affb274d32dc68ef1d11
Richard Kojedzinszky <krichy@cflinux.hu>
2010-09-14 20:23:16 +0200

    upgraded openssl to 0.9.8o

--- mk/openssl.mk
+++ mk/openssl.mk
@@ -18,8 +18,8 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 PKG := openssl
-SRC_FILENAME = openssl-0.9.8l.tar.gz
-EXTRACTED_DIR = openssl-0.9.8l
+SRC_FILENAME = openssl-0.9.8o.tar.gz
+EXTRACTED_DIR = openssl-0.9.8o
 DOWNLOAD_SITES = http://www.openssl.org/source/ \
 		$(CFLINUX_PACKAGES)
 
