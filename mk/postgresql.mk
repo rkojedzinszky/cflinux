96c5986d90bc222f8129b71cf935c9d5ee684bad
Benedek László <laci@laci-laptop.benedekl.info>
2011-02-06 09:59:49 +0100

    PostgreSQL upgrade to 9.0.3

--- mk/postgresql.mk
+++ mk/postgresql.mk
@@ -18,10 +18,11 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 PKG := postgres
-POSTGRES_VER = 8.4.4
+POSTGRES_VER = 9.0.3
 SRC_FILENAME = postgresql-$(POSTGRES_VER).tar.bz2
 EXTRACTED_DIR = postgresql-$(POSTGRES_VER)
 DOWNLOAD_SITES = \
+		http://wwwmaster.postgresql.org/redir/33/h/source/v$(POSTGRES_VER)/ \
 		http://wwwmaster.postgresql.org/redir/53/h/source/v$(POSTGRES_VER)/ \
 		http://wwwmaster.postgresql.org/redir/56/h/source/v$(POSTGRES_VER)/ \
 		http://wwwmaster.postgresql.org/redir/198/h/source/v$(POSTGRES_VER)/ \
