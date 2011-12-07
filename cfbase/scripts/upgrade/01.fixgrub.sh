f64ad66b6ee6a7d079e74985e650c81f44533d6c
Richard Kojedzinszky <krichy@tvnetwork.hu>
2011-12-07 16:36:51 +0100

    Upgraded fixgrub.sh to load 8192 sectors

--- cfbase/scripts/upgrade/01.fixgrub.sh
+++ cfbase/scripts/upgrade/01.fixgrub.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 
+SECTORS_TO_LOAD=8192
+
 ## this script is for upgrading the grub's meun.lst to
 ## load a kernel with size greater than 1MB
 set -e
 
-if grep -q "^kernel.*)1+4096" /boot/grub/menu.lst ; then
+if grep -q "^kernel.*)1+$SECTORS_TO_LOAD" /boot/grub/menu.lst ; then
 	exit 0
 fi
 
 echo "It seems that your grub menu entry has not been upgraded"
-echo "to be able to load a kernel with size greater than 1MB."
+echo "to be able to load a big kernel."
 echo "I can now alter your menu.lst to fit this needs, but you have"
 echo "to confirm it."
 
 tmp="grub.menu.lst.$$"
 
-sed 's/^\(kernel.*)1+\)[0-9]\+/\14096/' /boot/grub/menu.lst > $tmp
+sed 's/^\(kernel.*)1+\)[0-9]\+/\1'$SECTORS_TO_LOAD'/' /boot/grub/menu.lst > $tmp
 echo -e "\n--- NEW GRUB MENU.LST ---"
 cat $tmp
 echo -e "--- MENU.LST ends ---"
@@ -34,7 +36,7 @@ case "$ans" in
 	*)
 		rm -f "$tmp"
 		echo "Not replacing your menu.lst. You should edit it by hand to"
-		echo "load 4096 sectors of the kernel."
+		echo "load $SECTORS_TO_LOAD sectors of the kernel."
 		exit 1
 		;;
 esac
