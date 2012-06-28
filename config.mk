f46fd99c08666a4f93309f944bc3374ce557bf3c
Richard Kojedzinszky <krichy@tvnetwork.hu>
2012-06-28 13:03:42 +0200

    kernel upgraded to 3.2.21

--- config.mk
+++ config.mk
@@ -50,7 +50,7 @@ TARGET_RANLIB = $(TARGET_HOST)-ranlib
 TARGET_STRIP = $(TARGET_HOST)-strip -s
 
 # The version of the used kernel
-KERNEL_VERSION := 3.2.15
+KERNEL_VERSION := 3.2.21
 
 # the directory where uclibc will be installed
 UC_ROOT=$(FDEVEL_DIR)/$(TARGET_HOST)
