2fd1849f959b7e2986b8fae7566557a57b3b1c7a
Richard Kojedzinszky <krichy@cflinux.hu>
2013-11-24 14:12:53 +0100

    iptables upgraded to 1.4.21

--- mk/iptables.mk
+++ mk/iptables.mk
@@ -18,8 +18,8 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 PKG := iptables
-SRC_FILENAME = iptables-1.4.13.tar.bz2
-EXTRACTED_DIR = iptables-1.4.13
+SRC_FILENAME = iptables-1.4.21.tar.bz2
+EXTRACTED_DIR = iptables-1.4.21
 DOWNLOAD_SITES = http://www.netfilter.org/projects/iptables/files \
 		$(CFLINUX_PACKAGES)
 
